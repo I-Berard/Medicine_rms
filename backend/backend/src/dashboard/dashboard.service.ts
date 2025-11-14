11b1b85df7e8f0ad69b85995fcad16042c414eab
I-Berard <irakozeberard12@gmail.com>
2025-11-14 07:15:50 +0200

    added some stuff

--- backend/backend/src/dashboard/dashboard.service.ts
+++ backend/backend/src/dashboard/dashboard.service.ts
@@ -14,11 +14,11 @@ export class DashboardService {
     async dashboard(dashboardinput: DashboardDto): Promise<any> {
         const notifications: NotificationResponseDto[] = [];
         // const notification_data = {medicine_type: dashboardinput.medicine_type, medicine_name: dashboardinput.medicine_name, times_of_the_day: dashboardinput.times_of_the_day, interval_hours: dashboardinput.interval_hours, start_time: dashboardinput.start_time}
-        const medicine = await this.medicineService.findOneByUserId(dashboardinput.userId);
+        const medicine = await this.medicineService.findOneByUserId(dashboardinput.userId); // This one fetches a user 
         for (const med of medicine){
             for (const sch of med.schedule){
                 const notification_data: NotificationDto = {medicine_name: sch.medicine.name, medicine_type: sch.medicine_type, times_of_the_day: sch.times_of_the_day, interval_hours: sch.interval_hours, start_time: sch.start_time};
-                const notification = this.notificationService.getNotificationTimes(notification_data);
+                const notification = this.notificationService.getNotificationTimes(notification_data);// and later get's this user notification times for the day
                 notifications.push(notification);
             }
         }
